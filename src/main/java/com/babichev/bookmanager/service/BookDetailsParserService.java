f04e74e457940cd73316785fe44e14de41e1a640
Andrey Babichev <Al.94_94@mail.ru>
2021-04-08 09:35:50 +0300

    Make refactor for BoolDetailsService

---
+++ src/main/java/com/babichev/bookmanager/service/BookDetailsParserService.java
@@ -0,0 +1,132 @@
+package com.babichev.bookmanager.service;
+
+
+import com.babichev.bookmanager.entity.Book;
+import com.babichev.bookmanager.entity.Details;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+import static com.babichev.bookmanager.util.StringConverter.*;
+
+@Service
+public class BookInfoParserService implements InfoParserService {
+    private final String SEARCH_PAGE_URL = "https://openlibrary.org/search?q=%s&mode=everything";
+    private final String INFO_MAIN_URL = "https://openlibrary.org";
+
+    private BookService bookService;
+
+    @Autowired
+    public BookInfoParserService(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+
+    @Override
+    public Details findInfoOnPage(Book book) {
+
+        String searchingParam = buildSearchingParam(book);
+        String formattedUrl = String.format(SEARCH_PAGE_URL, searchingParam);
+
+        String detailsLink = parseDocumentToGetDetailsLink(formattedUrl);
+        Details details = getDetails(detailsLink);
+
+        return details;
+    }
+
+    //BUILD A STRING TO ADD AS PARAM FOR FORMATTER FOR INFO_URL STRING
+    private String buildSearchingParam(Book book) {
+        String name = book.getName();
+        String author = book.getAuthor();
+
+        name = name == null || name.equals("")? "" : getSplittedString(name);
+        author = author == null || author.equals("")? "" : getSplittedString(author);
+
+        return name + author;
+    }
+
+
+    private String parseDocumentToGetDetailsLink(String formattedUrl) {
+        String detailsLink = null;
+
+        try {
+            Document document = Jsoup.connect(formattedUrl).get();
+
+            Element contentBody = document.getElementById("searchResults");
+            Elements searchResultItem = contentBody.getElementsByClass("searchResultItem");
+            Element firstBookFromSearch = searchResultItem.first();
+
+            String linkToDetailsPage = firstBookFromSearch.getElementsByClass("booktitle")
+                    .first()
+                    .select("a")
+                    .first()
+                    .attr("href");
+
+            //CREATE DETAIL INFO LINK
+            detailsLink = INFO_MAIN_URL + linkToDetailsPage;
+
+        } catch (IOException e) {
+            e.getStackTrace();
+        } catch (NullPointerException e) {
+            e.getStackTrace();
+        }
+
+        return detailsLink;
+    }
+
+    private Details getDetails(String link) {
+        Details details = new Details();
+
+        try {
+            Document document = Jsoup.connect(link).get();
+
+            Elements bookDescriptionClass = document.getElementsByClass("workDetails");
+
+            String description = getDescription(bookDescriptionClass);
+            String imgLink = getImageLink(bookDescriptionClass);
+
+            //SET DETAILS FIELDS
+            details.setDescription(description);
+            details.setImage(imgLink);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return details;
+    }
+
+    private String getDescription(Elements bookDescriptionClass) {
+        String description = bookDescriptionClass
+                .first()
+                .getElementsByClass("editionAbout")
+                .first()
+                .getElementsByClass("book-description")
+                .first()
+                .text();
+
+        description = sliceTheDescription(
+                sliceTheDescription(description,  "Read less"),
+                "Read more");
+
+        return description;
+    }
+
+    private String getImageLink(Elements bookDescriptionClass) {
+        String imgLink = bookDescriptionClass
+                .first()
+                .getElementsByClass("editionCover")
+                .first()
+                .select("a")
+                .first()
+                .attr("href");
+
+        return imgLink;
+    }
+
+}
--- src/main/java/com/babichev/bookmanager/util/StringConverter.java
+++ src/main/java/com/babichev/bookmanager/util/StringConverter.java
@@ -2,13 +2,13 @@ package com.babichev.bookmanager.util;
 
 public class StringConverter {
 
-    public static String parseDescription(String str, String replaced) {
-        String result = str;
+    public static String sliceTheDescription(String str, String replaced) {
+        String result = null;
         result = str.replaceAll(replaced.trim(), "");
         return result;
     }
 
-    //Method for splitting the string to valid for searching,
+    //Method for splitting the string to valid it for searching,
     //if the string is complicated(i.e. The Hobbit)
     //we need to transform it to "The+Hobbit"
     public static String getSplittedString(String str) {

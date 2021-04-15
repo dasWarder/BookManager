82a52291ca7bdb20bc0eb0631ba0216bf4f344d8
Andrey Babichev <Al.94_94@mail.ru>
2021-04-15 22:19:19 +0300

    Fix some view elements, add back to customer list button

--- src/main/java/com/babichev/bookmanager/controller/CustomerController.java
+++ src/main/java/com/babichev/bookmanager/controller/CustomerController.java
@@ -6,7 +6,6 @@ import com.babichev.bookmanager.service.CustomerService;
 import com.babichev.bookmanager.util.SecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
